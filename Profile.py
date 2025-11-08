81aecc580d9c772ff4396972add68c3a1e00f021
Your Name <you@example.com>
2025-11-08 15:46:09 -0500

    Profile class

---
+++ Profile.py
@@ -0,0 +1,19 @@
+class Profile:
+    """Establishing a profile for the user."""
+    username: str
+    bio: str
+    socials: str
+
+    def __init__(self, username: str, bio: str, socials: str):
+        self.username = username
+        self.bio = bio
+        self.socials = socials
+
+    def edit_bio(self):
+        self.bio = input("Update bio.")
+
+    def edit_username(self):
+        self.username = input("Update username.")
+
+def upload_img():
+    
